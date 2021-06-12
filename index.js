ededbc6bd115cfdae7332b7e105c60d56b1321db
Samantha Ostrowski <isphinxs@hotmail.com>
2021-06-11 22:54:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,63 @@
 /* Your Code Here */
 
+const createEmployeeRecord = (arr) => {
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+const createEmployeeRecords = arrOfArrs => {
+    return arrOfArrs.map(arr => createEmployeeRecord(arr));
+}
+
+// const createTimeInEvent = dateStamp => {
+//     const dateSections = dateStamp.split(" ");
+//     const newEntry = {
+//         type: "TimeIn",
+//         hour: dateSections[1] / 100,
+//         date: dateSections[0]
+//     }
+//     // debugger;
+//     this.timeInEvents.push(newEntry);
+//     return this;
+// }
+
+function createTimeInEvent(dateStamp) {
+    const dateSections = dateStamp.split(" ");
+    const newEntry = {
+        type: "TimeIn",
+        hour: parseInt(dateSections[1]),
+        date: dateSections[0]
+    }
+    this.timeInEvents.push(newEntry);
+    return this;
+}
+
+function createTimeOutEvent (dateStamp) {
+    const dateSections = dateStamp.split(" ");
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(dateSections[1]),
+        date: dateSections[0]
+    });
+    return this;
+}
+
+function hoursWorkedOnDate(dateStamp) {
+    const timeIn = this.timeInEvents.find(entry => entry.date === dateStamp).hour / 100;
+    const timeOut = this.timeOutEvents.find(entry => entry.date === dateStamp).hour / 100;
+    return parseInt(timeOut, 10) - parseInt(timeIn, 10);
+}
+
+function wagesEarnedOnDate(dateTime) {
+    return hoursWorkedOnDate.call(this, dateTime) * this.payPerHour;
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -19,4 +77,17 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName);
+}
+
+function calculatePayroll(employees) {
+
+    const test = employees.reduce((memo, employee) => {
+        return memo + allWagesFor.call(employee);
+    }, 0)
+    console.log(test);
+    return test;
 }
--- test/helpers.js
+++ test/helpers.js
@@ -7,14 +7,14 @@ const path = require('path')
 const babel = require('babel-core');
 const spies = require('chai-spies')
 
-chai.use( spies );
+chai.use(spies);
 
 const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
 
 const babelResult = babel.transformFileSync(
   path.resolve(__dirname, '..', 'index.js'), {
-    presets: ['env']
-  }
+  presets: ['env']
+}
 );
 
 const src = babelResult.code
